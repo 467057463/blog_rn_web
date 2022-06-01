75afb91e47455992ddcc7772c2ef5aeb698b76ec
毛有文 <467057463@qq.com>
2022-06-01 17:16:49 +0800

    继续尝试共建方案

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
  import React from 'react';
- import { NavigationContainer } from '@react-navigation/native';
+ import { NavigationContainer, TabRouter } from '@react-navigation/native';
  import { createNativeStackNavigator } from '@react-navigation/native-stack';
  import {
    SafeAreaView,
@@ -11,15 +11,42 @@
    View,
  } from 'react-native';
  import Home from './views/Home'
- import Login from './views/Login'
+ import Details from './views/Details'
 
-const Stack = createNativeStackNavigator();
+ export type RootStackParamList = {
+  Home: undefined;
+  Details: {
+    id: number
+  };
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
  const App = () => { 
    return (
     <NavigationContainer>    
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home}/>
-        <Stack.Screen name="Login" component={Login}/>
+      <Stack.Navigator 
+        initialRouteName='Home'
+        screenOptions={{
+          headerTitleAlign: 'center',
+          headerTintColor: '#fff',
+          headerStyle: {
+            backgroundColor: '#f4511e'
+          },
+        }}
+      >
+        <Stack.Screen 
+          name="Home" 
+          component={Home} 
+          options={{
+            title: '首页',            
+          }}
+        />
+        <Stack.Screen name="Details" 
+          component={Details}
+          options={({route}) => ({
+            title: String(route.params.id)
+          })}
+        />
       </Stack.Navigator>
     </NavigationContainer>
    );
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,16 +1,29 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 
-export default function Home(){
+import type { RootStackParamList } from '../App'
+
+type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
+export default function Home({navigation}:Props){
   return(
-    <View>
-      <Text>
-        abc <Text>App.tsx</Text> to change this
-        screen and then come back to see your edits.
-      </Text>
-      <Text>
-        Read the docs to discover what to do next:
-      </Text>
+    <View style={styles.view}>
+      <Text>Home screen</Text>
+      <Button 
+        title="go to Details"
+        onPress={() => navigation.navigate('Details', {
+          id: 1111
+        })}
+      />
+      
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  }
+})
