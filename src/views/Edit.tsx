1409206e0ba77fd4964ea2c7e8b2aa9957a93492
毛有文 <467057463@qq.com>
2022-07-15 18:33:58 +0800

    编辑文章

--- src/views/Edit.tsx
+++ src/views/Edit.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { observer } from 'mobx-react-lite';
 import { View } from 'react-native';
-import { Text } from '@rneui/themed';
+import { Input } from '@rneui/themed';
 import Editor from '@/components/Editor';
 
 export default observer(function Edit() {
   return (
-    <View style={{ flex: 1, backgroundColor: 'red' }}>
+    <View style={{ flex: 1 }}>
+      <Input
+        placeholder="请输入文章标题"
+        errorStyle={{ display: 'none' }}
+        inputContainerStyle={{ borderBottomWidth: 0 }}
+      />
       <Editor />
     </View>
   );
