65d2f01f0709eef64552481f756b8ca498a494b1
m2 <467057463@qq.com>
2022-08-04 14:25:03 +0800

    change deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -20,9 +20,9 @@ module.exports = {
       ref: 'origin/main',
       repo: 'git@github.com:467057463/blog_rn_web.git',
       path: '/var/www/blog_rn_web',
-      'pre-deploy-local': '',
-      'post-deploy':
-        'npm --production=false install --legacy-peer-deps && cp ./icons/Iconfont.js ./node_modules/react-native-vector-icons/ && cp ./icons/Iconfont.json ./node_modules/react-native-vector-icons/glyphmaps/ && cp ./icons/Iconfont.ttf ./node_modules/react-native-vector-icons/Fonts/ && npm run build:web',
+      // 'pre-deploy-local': '',
+      // 'post-deploy':
+      //   'npm --production=false install --legacy-peer-deps && cp ./icons/Iconfont.js ./node_modules/react-native-vector-icons/ && cp ./icons/Iconfont.json ./node_modules/react-native-vector-icons/glyphmaps/ && cp ./icons/Iconfont.ttf ./node_modules/react-native-vector-icons/Fonts/ && npm run build:web',
       'pre-setup': '',
       env: {
         NODE_ENV: 'production',
